625878c02cbaaed6f251ffd4bc1a23a01fe0d890
Home <home@Homes-MacBook-Pro.local>
2024-07-28 23:02:15 +0530

    backup: initial commit.

--- manally_backpropagation.py
+++ manally_backpropagation.py
@@ -4,4 +4,7 @@ from neural_network import Value
 #NOTE: We need to do manually backprogation to neuron.
 #   TODO: Create a Neural Network of simple expression with two inputs(x1, x2) respectively.
 #   TODO: Multiple the  synapse(weights w) to the inputs(x1, x2) of the neural networks.
-#   TODO: Create a cell body for the Neural Network (sum of all inputs to the neuron with weights multiplied to it cell_body =x1w1+x2w2+...)
+#   TODO: Create a cell body for the Neural Network
+#   (sum of all inputs to the neuron with weights multiplied to it cell_body = x1w1+x2w2+...)
+
+
